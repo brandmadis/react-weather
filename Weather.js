f267d095c70733423eb95c533a90341141dfc64d
Brandon Madison <brandmadis@gmail.com>
2020-09-30 11:10:37 -0700

    Init react weather

---
+++ Weather.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Covid extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather</h1>
+      </div>
+    );
+  }
+}
+
+export default Covid;
